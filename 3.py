3df566679856911baadd7cf84657f45e682d4c5e
YG0811 <144429704+YG0811@users.noreply.github.com>
2023-12-08 15:11:33 +0200

    Delete 3.py

--- 3.py
+++
@@ -1,5 +0,0 @@
-keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except']
-
-new_keywords = [word for word in keywords if len(word) >= 5]
-
-print(new_keywords)
