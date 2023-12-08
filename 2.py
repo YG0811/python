b690a01cdf2a43fca175dd1ab81a61ea71b93c3a
YG0811 <144429704+YG0811@users.noreply.github.com>
2023-12-08 15:11:17 +0200

    Delete 2.py

--- 2.py
+++
@@ -1,5 +0,0 @@
-keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except']
-
-lengths = [len(word) for word in keywords]
-
-print(lengths)
