2e77ec481b8d5537e260c8bc0084302b4aa3f450
YG0811 <144429704+YG0811@users.noreply.github.com>
2023-12-08 15:11:08 +0200

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except']
-new_keywords = [word[1:] for word in keywords]
-
-print(new_keywords)
